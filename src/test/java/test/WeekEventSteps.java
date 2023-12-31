c888b9c5091654e08f93d947e0b47a01921f0ce3
Ashraf152 <ashraf.egbaria@gmail.com>
2023-12-31 19:10:15 +0200

    create steps for add new event and for change the first day of the week to Monday"

---
+++ src/test/java/test/WeekEventSteps.java
@@ -0,0 +1,92 @@
+package test;
+
+import Infrastructure.AppiumWrapper;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import logic.Sittings;
+import logic.WeekEvent;
+import org.junit.Assert;
+import utils.TestContext;
+
+import java.net.MalformedURLException;
+
+public class WeekEventSteps {
+
+    private WeekEvent weekEvent;
+    private static AndroidDriver<MobileElement> driver;
+    private final TestContext context;
+    private Sittings sittings;
+
+    public WeekEventSteps(TestContext context) throws MalformedURLException {
+        this.context = context;
+    }
+
+    @Given("i am on week page")
+    public void iAmOnWeekPage() throws MalformedURLException {
+        AppiumWrapper appiumWrapper = new AppiumWrapper();
+        driver= appiumWrapper.getDriver();
+        context.put("driver",driver);
+        weekEvent=new WeekEvent(context.get("driver"));
+    }
+
+    @When("i click on plus icon")
+    public void iClickOnPlusIcon() {
+        weekEvent.clickOnPlusButton();
+    }
+
+    @And("i click on today button")
+    public void iClickOnTodayButton() {
+        weekEvent.clickOnTodayButton();
+    }
+
+    @And("i fill a event {string}")
+    public void iFillEventaName(String eventName) {
+        weekEvent.fillEventName(eventName);
+    }
+
+    @And("i click save")
+    public void iClickSave() {
+        weekEvent.ckickSaveButton();
+    }
+
+    @Then("validate the event {string} is added")
+    public void validateTheEventIsAdded(String eventName) {
+        Assert.assertEquals(weekEvent.checkPendiningEvent(),eventName);
+    }
+
+
+    @When("i click on menu")
+    public void iClickOnMenu() {
+        weekEvent.clickOnMenu();
+    }
+
+    @And("i click on settings button")
+    public void iClickOnSettingsButton() {
+        weekEvent.clickOnSittingsButton();
+    }
+
+    @And("i click on first day of the week")
+    public void iClickOnFirstDayOfTheWeek() {
+        sittings=new Sittings(context.get("driver"));
+        sittings.clickOnFirstDayOfTheWeek();
+    }
+
+    @And("i chose monday to be the first day of the week")
+    public void iChoseMondayToBeTheFirstDayOfTheWeek() {
+        sittings.choseFirstDayOfTheWeek();
+    }
+
+    @And("click on back button")
+    public void clickOnBackButton() {
+        sittings.clickBackButton();
+    }
+
+    @Then("verify that monday is the first day of the week")
+    public void verifyThatMondayIsTheFirstDayOfTheWeek() {
+        Assert.assertEquals(weekEvent.checkFirstDayOfTheWeek(),"Mon");
+    }
+}
--- src/test/resources/WeekEvent.feature
+++ src/test/resources/WeekEvent.feature
@@ -6,13 +6,14 @@ Feature: Adding/ remove Events
     Scenario: verify adding a new event
       When i click on plus icon
       And i click on today button
-      And i fill event name
+      And i fill a event 'name'
       And i click save
-      Then validate the event is added
+      Then validate the event 'name' is added
 
-      Scenario: Add time event
-        When i ckick on 4 Thu , 11:00 rectangle
-        And i click new event
-        And i fill event 'name'
-        And i click save
-        Then event 'name' should be displayed and the same rectangle
+    Scenario: change the first day of the week to Monday
+      When i click on menu
+      And i click on settings button
+      And i click on first day of the week
+      And i chose monday to be the first day of the week
+      And click on back button
+      Then verify that monday is the first day of the week
